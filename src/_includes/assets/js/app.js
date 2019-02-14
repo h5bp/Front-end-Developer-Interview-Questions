d50efe27f9e07dbea1b6ba8ec49918e4b5a107e7
James Steinbach <jdsteinbach@gmail.com>
2019-02-13 17:20:18 -0700

    Make nav responsive: hamburger icon, js toggle, flexible styles

--- src/_includes/assets/js/app.js
+++ src/_includes/assets/js/app.js
@@ -1,4 +1,28 @@
 console.log(
   '%cA black belt is a white belt that didn\'t quit.',
-  'color: #fff; font-size: 80px; text-shadow: 1px 1px 1px #000; font-family: serif;'
+  'color: #fff; font-size: 20px; text-shadow: 1px 1px 1px #000; font-family: serif;'
 )
+
+var navToggle = document.getElementById('navigation-toggle')
+var navQuestions = document.getElementById('navigation-questions')
+
+navToggle.addEventListener('click', function() {
+  if (navQuestions.classList.contains('active')) {
+    navQuestions.classList.remove('active')
+  } else {
+    navQuestions.classList.add('active')
+  }
+})
+
+window.addEventListener('resize', function() {
+  if (document.width > 960) {
+    // Remove `active` class if viewport shows full menu
+    navQuestions.classList.remove('active')
+  } else {
+    // Temporarily add class to prevent visible motion after resize
+    navQuestions.classList.add('no-motion')
+    setTimeout(function () {
+      navQuestions.classList.remove('no-motion')
+    }, 250);
+  }
+})
